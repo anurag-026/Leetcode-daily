0b14e0537988d9f64eb9981a27c0d15f4f2f73ee
Anurag Mishra <anurag026mishra@gmail.com>
2024-09-05 12:53:56 +0530

    Time: 2 ms (52.35%), Space: 7.3 MB (79.27%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t[38];
+        t[0]=0;
+        t[1]=1;
+        t[2]=1;
+
+        for(int i=3 ; i<=37 ; i++){
+            t[i] = t[i-1] + t[i-2] + t[i-3];
+        }
+        return t[n];
+    }
+};
