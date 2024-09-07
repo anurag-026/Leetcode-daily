a2d9bd867ee73850121991efec8d7d3ed67e095d
Anurag Mishra <anurag026mishra@gmail.com>
2024-09-07 11:50:09 +0530

    Time: 9 ms (35.71%), Space: 10.7 MB (49.41%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rad, dir;
+        int n = senate.length();
+         for (int i = 0; i < n; i++){
+            if (senate[i] == 'R'){
+                rad.push(i);
+            }
+            else {
+                dir.push(i);
+            }
+        }
+        // Use increasing n to keep track of position
+        while (!rad.empty() && !dir.empty()){
+            // Only "winner" stays in their queue
+            if (rad.front() < dir.front()){
+                rad.push(n++);
+            }
+            else {
+                dir.push(n++);
+            }
+            rad.pop(), dir.pop();
+        }
+        return (rad.empty()) ? ("Dire") : ("Radiant");
+    }
+};
