edf8ac0d5f529837ad4da7ca69c68aa8384c91d8
Anurag Mishra <anurag026mishra@gmail.com>
2024-09-18 18:25:00 +0530

    Time: 3 ms (93.74%), Space: 17.4 MB (36.02%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -4,14 +4,18 @@ public:
         vector<string> array;
         for (int num : nums) {
             array.push_back(to_string(num));
-        } 
+        }
+ 
         sort(array.begin(), array.end(), [](const string &a, const string &b) {
             return (b + a) < (a + b);
         });
+
+        for(int i = 0; i< array.size() ; i++)cout<<array[i]<<" "; 
  
         if (array[0] == "0") {
             return "0";
-        } 
+        }
+ 
         string largest;
         for (const string &num : array) {
             largest += num;
