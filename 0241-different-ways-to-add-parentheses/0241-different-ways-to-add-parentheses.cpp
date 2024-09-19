6801babacca2d8e516c94d7e73a0c009ace73a96
Anurag Mishra <anurag026mishra@gmail.com>
2024-09-19 09:06:14 +0530

    Time: 0 ms (100%), Space: 9 MB (91.46%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string exp) { 
+        unordered_map<string, vector<int> > memo;
+        
+        return helper(exp, memo);
+    }
+    vector<int> helper(string exp, unordered_map<string, vector<int> > & memo) {
+        
+        if(memo.find(exp) != memo.end()) {
+            return memo[exp];
+        }
+        
+        vector<int> ans;
+        
+        const int size = exp.size();
+        
+        for(int i = 0; i < size; ++i) {
+            if('+' == exp[i] || '-' == exp[i] || '*' == exp[i]) {
+                vector<int> ansl = helper(exp.substr(0, i), memo );   
+                vector<int> ansr = helper(exp.substr(i+1), memo ); 
+                
+                for(int l : ansl) {
+                    for(int r : ansr) {
+                        int curAns;
+                        if('+' == exp[i]) {
+                            curAns = l + r; 
+                        } else if ('-' == exp[i]) { 
+                            curAns = l - r;
+                        } else { 
+                            curAns = l * r;
+                        }
+                        ans.push_back(curAns);
+                    }
+                }
+                
+            }
+         }
+        
+        if(0 == ans.size()) {
+            ans.push_back(atoi(exp.c_str()));
+        }
+
+        memo[exp] = ans;
+        return ans;
+    }
+};
