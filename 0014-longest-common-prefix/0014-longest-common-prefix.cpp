410ccb8ef0a2aaeb8154251ea26078f7a22de3bb
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-01 14:24:31 +0530

    Time: 7 ms (27.06%), Space: 18.7 MB (5.62%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
    string longestCommonPrefix(vector<string> arr) {
-        string ms=arr[0];
+        // string ms=arr[0];
+        // int n= arr.size();
+        // for(int i=1; i<n; i++)
+        //     if(arr[i].size() < ms.size() )    ms = arr[i];            
         
-        int n= arr.size();
+        // string temp="";
+        // int i=0;
+        // while(i<n){
+        //     temp="";
+        //     string s = arr[i];
+        //     for(int j=0; j<s.size() &&  j<ms.size(); j++){                
+        //         if(s[j] == ms[j])    temp= temp+s[j];                
+        //         else break;                
+        //     }
+        //     if(temp.size() ==0) return "";
+        //     if(temp.size() < ms.size())   ms = temp;
+        //     i++;
+        // }
+        // return  ms;
+
+
+        string psum="";
         
-        for(int i=1; i<n; i++){
-            if(arr[i].size() < ms.size() ){
-                ms = arr[i];
-            }
-        }
-        string temp="";
-        int i=0;
-        while(i<n){
-            temp="";
-            string s = arr[i];
-            for(int j=0; j<s.size() &&  j<ms.size(); j++){
-                //if(s == ms) break;
-                if(s[j] == ms[j]){
-                    temp= temp+s[j];
+        for(int i=0; i<arr[0].size() ; i++){
+            for( auto s :arr){
+                if(s[i] != arr[0][i]){
+                    return psum;
                 }
-                else{
-                    break;
-                }
-                
             }
-            if(temp.size() ==0) return "";
-            if(temp.size() < ms.size()){
-                ms = temp;
-            }
-            i++;
+            psum += arr[0][i];
         }
-        return  ms;
+        return psum;
         
 
     }
