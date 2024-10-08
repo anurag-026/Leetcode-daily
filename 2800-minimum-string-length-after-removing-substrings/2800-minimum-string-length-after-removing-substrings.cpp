3b4c6339bf1a7fd5ddba0891004fd7e38689a3c8
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-08 16:33:09 +0530

    Time: 11 ms (19.54%), Space: 10 MB (94.54%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minLength(string s) {
+        while (s.find("AB") != std::string::npos || s.find("CD") != std::string::npos) {
+            if (s.find("AB") != std::string::npos) {
+                s.erase(s.find("AB"), 2);  
+            }
+            if (s.find("CD") != std::string::npos) {
+                s.erase(s.find("CD"), 2);  
+            }
+        }
+        return s.length();
+    }
+};
