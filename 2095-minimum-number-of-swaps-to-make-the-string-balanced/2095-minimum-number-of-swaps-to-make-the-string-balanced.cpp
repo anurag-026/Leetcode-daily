a86d9e5fb4dd159097f0bb73a5c605c420338033
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-08 21:56:48 +0530

    Time: 43 ms (99.57%), Space: 32.6 MB (91.34%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int minSwaps(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int ans = 0;
         for (auto c:s){
             if( c == '[')
                 ans += 1;
             else if(ans > 0)
                 ans -= 1;
-            
         }
         return (int)((ans + 1) / 2);
             
