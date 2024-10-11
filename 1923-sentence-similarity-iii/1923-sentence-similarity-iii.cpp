3972856e21e2846dc1c7cf87e2fefc3dac660333
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-11 22:59:12 +0530

    Time: 4 ms (41.47%), Space: 8.4 MB (72.34%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -19,23 +19,22 @@ public:
             return words;
         };
 
-        // Split both sentences into words
+         
         vector<string> words1 = splitWords(sentence1);
         vector<string> words2 = splitWords(sentence2);
         
-        // Ensure words1 is the longer sentence
+        
         if (words1.size() < words2.size()) swap(words1, words2);
         
         int start = 0, end = 0;
         int n1 = words1.size(), n2 = words2.size();
-        
-        // Compare from the start
+         
         while (start < n2 && words1[start] == words2[start]) start++;
         
-        // Compare from the end
+        
         while (end < n2 && words1[n1 - end - 1] == words2[n2 - end - 1]) end++;
         
-        // Check if the remaining unmatched part is in the middle
+       
         return start + end >= n2;
     }
 };
