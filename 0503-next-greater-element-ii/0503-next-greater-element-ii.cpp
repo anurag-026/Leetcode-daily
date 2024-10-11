7b58324e6ccbcbcc4bf136be80ec35e78bda11ba
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-12 00:51:22 +0530

    Time: 23 ms (56.42%), Space: 26.8 MB (59.49%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
+    // vector<int> nextGreaterElements(vector<int>& nums) {
+        // int n= nums.size();
+        // vector<int> nums2 ;
+        // for(int i=0; i<n; i++){
+        //     nums2.push_back(nums[i]) ;
+        // }
+        // for(int i=0; i<n; i++){
+        //     nums2.push_back(nums[i]) ;
+        // }
+
+        // n=2*n;
+        // stack<int> st;
+        // vector<int> ans(n);
+
+        // for(int i=n-1; i>=0; i--){
+        //     while(!st.empty() && st.top() <= nums2[i] ){
+        //         st.pop();
+        //     }
+        //     if( st.empty() ){
+        //           ans[i] = -1;
+        //     }
+        //     else if( !st.empty() && st.top() > nums2[i] ){
+        //         ans[i] = st.top();
+        //     }
+        //     st.push(nums2[i]);
+        // }
+        // ans.erase(ans.begin()+n/2, ans.end() );
+        // return ans;
+    // }
+
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n= nums.size();
-        vector<int> nums2 ;
-        for(int i=0; i<n; i++){
-            nums2.push_back(nums[i]) ;
-        }
-        for(int i=0; i<n; i++){
-            nums2.push_back(nums[i]) ;
-        }
-
-        n=2*n;
+        int n = nums.size();
+        vector<int> ans(n,-1);
         stack<int> st;
-        vector<int> ans(n);
-
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.top() <= nums2[i] ){
+        for( int i= 2*n-1; i>=0; i--){
+            int curr = nums[i%n];
+            while(!st.empty() && st.top() <= curr ){
                 st.pop();
             }
-            if( st.empty() ){
-                  ans[i] = -1;
-            }
-            else if( !st.empty() && st.top() > nums2[i] ){
-                ans[i] = st.top();
-            }
-            st.push(nums2[i]);
+             
+            if(!st.empty()) 
+                ans[i%n] = st.top();
+            
+            st.push(curr);
         }
-        ans.erase(ans.begin()+n/2, ans.end() );
         return ans;
     }
 };
