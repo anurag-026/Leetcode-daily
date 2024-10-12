f4edbf6e9ce7e9182fae1024acfa0f5d56c8f25c
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-13 00:14:38 +0530

    Time: 14 ms (46.98%), Space: 24.8 MB (88.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,3 +1,45 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n =  height.size();
+//         vector<int> lmax(n,0);
+//          vector<int> rmax(n,0);
+//         stack<int> st;
+
+//         for(int i=0; i<n; i++){
+//             while(!st.empty() && height[i] >= st.top()){
+//                 st.pop();
+//             }
+            
+//             if(!st.empty()){
+//                 lmax[i] = st.top();
+//             }
+//             if(!st.empty()  && height[i] >st.top() ) 
+//                 st.push(height[i]);
+//         }
+//         for(int i=n-1; i>=0; i--){
+//             while(!st.empty() && height[i] >= st.top()){
+//                 st.pop();
+//             }
+//             if(!st.empty()){
+//                rmax[i] = st.top();
+//             }
+//             if( !st.empty() && height[i] >st.top() ) 
+//                 st.push(height[i]);
+//         }
+
+//         int ans = 0;
+//         for(int j =0; j<n; j++){
+//             int m = min(rmax[j] , lmax[j] ); 
+//                 ans = ans+ m - height[j];
+             
+//         }
+//         return ans;
+
+//     }
+// };
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
