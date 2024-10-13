e1706d84109de01fcd24c60700d82f88458d872c
Anurag Mishra <anurag026mishra@gmail.com>
2024-10-13 23:19:41 +0530

    Time: 39 ms (28.77%), Space: 19.4 MB (42.33%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int cnt=0;
+    void solve(vector<int>& nums, int n ){
+        if(n==1) return;
+        for(int i =0; i<n; i++){
+            if((n-1-i) <= nums[i] ){
+                cnt++;
+                solve(nums , i+1);
+                break;
+            }
+        }
+        if(n==1) return;
+    }
+    int jump(vector<int>& nums) {
+        int n= nums.size();
+        solve(nums, n);
+        return cnt;
+    }
+};
