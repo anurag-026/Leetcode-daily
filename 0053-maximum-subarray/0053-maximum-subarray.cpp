caa76cf4b61d549450c6ff8dbf8dec5166f3af48
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-16 23:30:14 +0530

    Time: 0 ms (100%), Space: 71.8 MB (16.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,28 +1,19 @@
-int init = [] {
-    std::ios::sync_with_stdio(0);
-    std::cin.tie(0);
-    return 0;
-}();
-
-
- 
-
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum= INT_MIN;
+        int n=nums.size();
+        int maxi=INT_MIN;
         int sum=0;
-        for(int i=0; i<nums.size(); i++){
+        int index=0;
+        for(int i=0; i<n; i++){
             sum+=nums[i];
-
-            if(max_sum<sum){
-                max_sum= sum;
-            } 
+            maxi = max(sum,maxi);
             if(sum<0){
                 sum=0;
+                index=i+1;
             }
+            
         }
-        return max_sum;
+        return maxi;
     }
 };
