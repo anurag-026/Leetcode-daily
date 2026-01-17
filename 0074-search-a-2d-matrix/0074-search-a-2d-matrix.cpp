dc2c396dda3a445b62704cc260019d5589e51a1e
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-17 18:34:10 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
+    bool bS(vector<int> v, int target) {
+        int l = 0, r = v.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (v[mid] == target) {
+                return true;
+            } else if (v[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
 
         for (int i = 0; i < m; i++) {
-            
-            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
+
+            if (matrix[i][0] <= target && matrix[i].back() >= target) {
                 // will  execute only once
-                for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == target)
-                        return 1;
-                }
+                return bS(matrix[i], target);
             }
         }
-        // TC <= O(m+n)
+        // TC <= O( m + log(n))
         return 0;
     }
 };
