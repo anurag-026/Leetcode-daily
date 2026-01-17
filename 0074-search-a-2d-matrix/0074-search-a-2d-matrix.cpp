9e13cb85964a6ab0d3aed32bfe93d3d1c8ddf4e6
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-17 18:18:24 +0530

    Time: 0 ms (100%), Space: 13.2 MB (76.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -10,22 +10,16 @@ public:
         //     return 0;
 
         for (int i = 0; i < m; i++) {
-            if (matrix[i][0] > target) {
-                r = i - 1;
-                break;
-            } else {
-                r = i;
-            }
-            if (matrix[i][0] == target) {
-                return 1;
+            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
+                
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == target)
+                        return 1;
+                }
+
             }
         }
-        if (r < 0)
-            return 0;
-        for (int j = 0; j < n; j++) {
-            if (matrix[r][j] == target)
-                return 1;
-        }
+
         return 0;
     }
 };
