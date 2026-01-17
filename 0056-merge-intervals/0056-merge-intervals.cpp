93a171c309c9eae92f65c3121ae77269fa472332
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-17 17:00:07 +0530

    Time: 0 ms (100%), Space: 23.9 MB (32.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,17 +3,32 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
 
+        vector<vector<int>> ans;
+        for (int i = 0; i < intervals.size(); i++) {
+
+            if (ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            } else {
+                ans.back()[1] = max(intervals[i][1] , ans.back()[1]);
+            }
+        }
+        return ans;
+    }
+
+    vector<vector<int>> merge1(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
         int s = intervals[0][0];
         int e = intervals[0][1];
 
         vector<vector<int>> ans;
         for (int i = 1; i < intervals.size(); i++) {
             if (e >= intervals[i][0]) {
-                e = max(e,intervals[i][1]);
+                e = max(e, intervals[i][1]);
             } else {
                 ans.push_back({s, e});
                 s = intervals[i][0];
-                 e = max(e,intervals[i][1]);
+                e = max(e, intervals[i][1]);
             }
         }
         ans.push_back({s, e});
