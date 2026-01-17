f4764af836340aea8b697b03e9b38572777a3691
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-17 18:09:53 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,20 +5,23 @@ public:
         int n = matrix[0].size();
         int r = 0;
 
-        if (m == 0 || n == 0 ||
-            (m == 1 && n == 1 && target != matrix[m - 1][n - 1]))
-            return 0;
+        // if (m == 0 || n == 0 ||
+        //     (m == 1 && n == 1 && target != matrix[m - 1][n - 1]))
+        //     return 0;
 
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] > target) {
                 r = i - 1;
                 break;
+            } else {
+                r = i;
             }
             if (matrix[i][0] == target) {
                 return 1;
             }
         }
-        if(r<0) return 0;
+        if (r < 0)
+            return 0;
         for (int j = 0; j < n; j++) {
             if (matrix[r][j] == target)
                 return 1;
