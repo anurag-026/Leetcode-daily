bd42ea3391c3e51092ecd04cadcd943a06768709
Anurag Mishra <anurag026mishra@gmail.com>
2026-01-21 22:44:18 +0530

    Time: 11 ms (6.8%), Space: 35.5 MB (5.83%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-
-    vector<int> DecToBin(int n){
+    vector<int> DecToBin(int n) {
         vector<int> ans;
-        while(n>0){ 
-            ans.push_back(n%2);
-            n=n/2;
+        while (n > 0) {
+            ans.push_back(n % 2);
+            n = n / 2;
         }
         int l = ans.size();
-        for(int i=0; i< l%4; i++){
+        for (int i = 0; i < l % 4; i++) {
             ans.push_back(0);
         }
-        
+
         return ans;
     }
 
-    int BinToDec(vector<int> v){
-        int ans=0;
-        //reverse(v.begin(), v.end());
-        for(int i=0; i<v.size() ; i++ ){
-            ans += v[i]*pow(2,i);
+    int BinToDec(vector<int> v) {
+        int ans = 0;
+        for (int i = 0; i < v.size(); i++) {
+            ans += v[i] * pow(2, i);
         }
         return ans;
-        
     }
 
     vector<int> minBitwiseArray(vector<int>& nums) {
@@ -36,15 +33,14 @@ public:
 
             vector<int> nBin = DecToBin(num);
 
-            for(int i=0; i<nBin.size(); i++){
-                if(nBin[i] ==0){
-                    nBin[i-1] =0; 
+            for (int i = 0; i < nBin.size(); i++) {
+                if (nBin[i] == 0) {
+                    nBin[i - 1] = 0;
                     int n = BinToDec(nBin);
                     ans.push_back(n);
                     break;
                 }
             }
-            
         }
 
         return ans;
