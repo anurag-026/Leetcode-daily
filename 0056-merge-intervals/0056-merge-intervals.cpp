6229b1e21622ba2f4ef1dc8c923cee47a878d475
Anurag Mishra <anurag026mishra@gmail.com>
2026-06-02 21:20:58 +0530

    Time: 5 ms (52.47%), Space: 23.8 MB (81.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,36 +2,19 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> ans;
-        for (int i = 0; i < intervals.size(); i++) {
-
-            if (ans.empty() || ans.back()[1] < intervals[i][0]) {
-                ans.push_back(intervals[i]);
-            } else {
-                ans.back()[1] = max(intervals[i][1] , ans.back()[1]);
+        int n = intervals.size();
+        int ps = intervals[0][0], pe = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            if (pe >= intervals[i][0]) {
+                pe = max(pe, intervals[i][1]);
+            } else if (pe < intervals[i][0]) {
+                ans.push_back({ps, pe});
+                ps = intervals[i][0];
+                pe = intervals[i][1];
             }
         }
-        return ans;
-    }
-
-    vector<vector<int>> merge1(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-
-        int s = intervals[0][0];
-        int e = intervals[0][1];
-
-        vector<vector<int>> ans;
-        for (int i = 1; i < intervals.size(); i++) {
-            if (e >= intervals[i][0]) {
-                e = max(e, intervals[i][1]);
-            } else {
-                ans.push_back({s, e});
-                s = intervals[i][0];
-                e = max(e, intervals[i][1]);
-            }
-        }
-        ans.push_back({s, e});
+        ans.push_back({ps, pe});
         return ans;
     }
 };
