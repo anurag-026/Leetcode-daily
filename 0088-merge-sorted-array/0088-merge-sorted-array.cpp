af9d508689391eb942d301c7427899e78a43c6f5
Anurag Mishra <anurag026mishra@gmail.com>
2026-06-02 22:56:06 +0530

    Time: 0 ms (100%), Space: 11.3 MB (99.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int x = m+n-1, i=m-1, j=n-1;
+        int midx = m - 1;
+        int nidx = n - 1;
+        int right = m + n - 1;
 
-        while(i>=0 && j>=0){
-            if(nums1[i] >nums2[j]){
-                nums1[x]=nums1[i];
-                nums1[i]=0;
-                i--;
-            }else{
-                nums1[x]=nums2[j];
-                nums2[j]=0;
-                j--;
+        while (nidx >= 0) {
+            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
+                nums1[right] = nums1[midx];
+                midx--;
+            } else {
+                nums1[right] = nums2[nidx];
+                nidx--;
             }
-            x--;
-        }
-        while(j>=0){
-            nums1[x--] = nums2[j--];
-        }
-
-    }
-
-    // TC= m * nlogn
-    void merge1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0;
-        while (i < m && j < n) {
-            if (nums1[i] > nums2[j]) {
-                swap(nums1[i], nums2[j]);
-            }
-            i++;
-            sort(nums2.begin(), nums2.end());
-        }
-        while (j < n) {
-            nums1[i++] = nums2[j++];
-        }
+            right--;
+        }        
     }
 };
