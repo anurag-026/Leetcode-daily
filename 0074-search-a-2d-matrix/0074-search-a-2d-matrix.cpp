cfb6832105dd84ed5f8be574bbe0bb1f39eb2adb
Anurag Mishra <anurag026mishra@gmail.com>
2026-06-05 00:32:51 +0530

    Time: 0 ms (100%), Space: 13.3 MB (43.88%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-    bool bS(vector<int> v, int target) {
-        int l = 0, r = v.size() - 1;
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (v[mid] == target) {
-                return true;
-            } else if (v[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return false;
-    }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int l = 0, r = m - 1;
+        int n = matrix.size(), m = matrix[0].size();
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        if(n==0 || m==0){
 
-            if (matrix[mid][0] <= target && matrix[mid].back() >= target) {
-                // will  execute only once
-                return bS(matrix[mid], target);
-            } else if (matrix[mid][0] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
         }
-        // TC <= O( log(m) + log(n)) --> 2 binary searchs
+        int row;
+        int i = 0, j = n- 1, mid;
+
+        while (i <= j) {                     // 0<=2
+            mid = i + (j - i) / 2; // m=1
+            
+            if (matrix[mid][0] == target) {
+                return true;
+            } else if (matrix[mid][0] > target) {
+                j = mid - 1;
+            } else if (matrix[mid][0] < target) {
+                i = mid+1;
+            }
+            
+        }
+        if(j < 0) return false;
+        
+        row = j;
+
+        int col;
+
+        i = 0, j = m - 1, mid;
+        while (i <= j) {
+            mid = i + (j - i) / 2;
+
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+
+            if (matrix[row][mid] > target) {
+                j = mid - 1;
+            }
+            if (matrix[row][mid] < target) {
+                i = mid + 1;
+            }
+           
+        }
         return false;
     }
 };
