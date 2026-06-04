5744777fcf36bf64f14e0b139f947a026fec7f8e
Anurag Mishra <anurag026mishra@gmail.com>
2026-06-05 00:34:53 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.88%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix1(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m = matrix[0].size();
 
-        if(n==0 || m==0){
-
+        if (n == 0 || m == 0) {
         }
         int row;
-        int i = 0, j = n- 1, mid;
+        int i = 0, j = n - 1, mid;
 
-        while (i <= j) {                     // 0<=2
+        while (i <= j) {           // 0<=2
             mid = i + (j - i) / 2; // m=1
-            
+
             if (matrix[mid][0] == target) {
                 return true;
             } else if (matrix[mid][0] > target) {
                 j = mid - 1;
             } else if (matrix[mid][0] < target) {
-                i = mid+1;
+                i = mid + 1;
             }
-            
         }
-        if(j < 0) return false;
-        
+        if (j < 0)
+            return false;
+
         row = j;
 
         int col;
@@ -42,8 +40,25 @@ public:
             if (matrix[row][mid] < target) {
                 i = mid + 1;
             }
-           
         }
         return false;
     }
+
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n - 1;
+        while(row < m && col >= 0) {
+            if(matrix[row][col] == target){
+                return true;}
+            if(matrix[row][col] > target){
+                col--;}
+            else{
+                row++;}
+        }
+        return false;
+    }
+
 };
