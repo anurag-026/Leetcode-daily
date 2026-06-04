8797d6f3de1fa6601a306438ec6a0c489146acd0
Anurag Mishra <anurag026mishra@gmail.com>
2026-06-04 23:46:49 +0530

    Time: 0 ms (100%), Space: 26.7 MB (73.86%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> nums;
-        for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[0].size(); j++){
-                nums.push_back(grid[i][j]);
+        int n = grid.size();
+        vector<int> track((n+1) *n);
+        int dup, sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+               
+                if (track[grid[i][j]] == 1) {
+                    dup = grid[i][j];
+                }
+                sum += grid[i][j];
+                track[grid[i][j]] = 1;
             }
         }
-
-        int n=nums.size();
-        vector<int> v(n+1, 0);
-        int repeatedNum, sum=0;
-        for(int i=0; i<n;i++){
-            if(v[nums[i]] >0){
-                repeatedNum = nums[i];
-            }
-            sum+=nums[i];
-            v[nums[i]]++;
-        }
-        int missingNum = (n*(n+1)/2)-sum+repeatedNum;
-        return {repeatedNum, missingNum}; 
+        n= n*n;
+        int total = n * (n + 1) / 2;
+        int missing = total - sum + dup;
+        return {dup, missing};
     }
 };
